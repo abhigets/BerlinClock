3ee198b83102ad1c96276469e2cc916981741f84
Andrei Bogomja <bogomja@gmail.com>
2019-09-30 18:53:48 +0100

    Refactored tests

--- src/test/java/NextBiggestNumber/BerlinClockShould.java
+++ src/test/java/NextBiggestNumber/BerlinClockShould.java
@@ -3,70 +3,48 @@ package NextBiggestNumber;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
-public class BerlinClockShould
-{
-    BerlinClock myBerlinClock;
+public class BerlinClockShould {
+
+    private BerlinClock myBerlinClock;
+
     @BeforeEach
-    public void setup(){
+    public void setup() {
         myBerlinClock = new BerlinClock();
     }
 
-    @Test
-    public void return_all_ligts_off_when_input_00_00_00() {
-        assertArrayEquals(
-                new String[]{ "O","OOOO","OOOO","OOOOOOOOOOO","OOOO"},
-                myBerlinClock.getBerlinClock("00:00:00")
-        );
-    }
+    @ParameterizedTest
+    @CsvSource({
+            "O, OOOO, OOOO, OOOOOOOOOOO, OOOO, 00:00:00",
+            "Y, OOOO, OOOO, OOOOOOOOOOO, OOOO, 00:00:01",
+            "Y, OOOO, OOOO, OOOOOOOOOOO, YOOO, 00:01:01",
+            "Y, OOOO, OOOO, OOOOOOOOOOO, YYOO, 00:02:01",
+            "Y, OOOO, OOOO, OOOOOOOOOOO, YYYO, 00:03:01",
+            "Y, OOOO, OOOO, OOOOOOOOOOO, YYYY, 00:04:01"
+    })
+    void testWithParameters(String sec,
+                            String fiveFullHrs,
+                            String oneFullHr,
+                            String fiveFullMins,
+                            String oneFullMin,
+                            String berlinClockInput) {
 
-    @Test
-    public void given_I_enter_a_00_00_01_i_should_get_representation_of_01() {
         assertArrayEquals(
-                new String[]{ "Y","OOOO","OOOO","OOOOOOOOOOO","OOOO"},
-                myBerlinClock.getBerlinClock("00:00:01")
+                new String[] { sec, fiveFullHrs, oneFullHr, fiveFullMins, oneFullMin },
+                myBerlinClock.getBerlinClock(berlinClockInput)
         );
-    }
 
-    @Test
-    public void given_I_enter_a_00_01_01_i_should_get_representation_of_1_minute_and_1_second() {
-        assertArrayEquals(
-                new String[]{ "Y","OOOO","OOOO","OOOOOOOOOOO","YOOO"},
-                myBerlinClock.getBerlinClock("00:01:01")
-        );
-    }
-
-    @Test
-    public void return_YYOO_for_2_minutes() {
-        assertArrayEquals(
-                new String[]{ "Y","OOOO","OOOO","OOOOOOOOOOO","YYOO"},
-                myBerlinClock.getBerlinClock("00:02:01")
-        );
-    }
-
-    @Test
-    public void return_YYYO_for_3_minutes() {
-        assertArrayEquals(
-                new String[]{ "Y","OOOO","OOOO","OOOOOOOOOOO","YYYO"},
-                myBerlinClock.getBerlinClock("00:03:01")
-        );
-    }
-
-    @Test
-    public void return_YYYY_for_4_minutes() {
-        assertArrayEquals(
-                new String[]{ "Y","OOOO","OOOO","OOOOOOOOOOO","YYYY"},
-                myBerlinClock.getBerlinClock("00:04:01")
-        );
     }
 
     @Test
     @Disabled
     public void given_I_enter_a_01_01_01_i_should_get_representation_of_1_hour_1_minute_and_1_second() {
         assertArrayEquals(
-                new String[]{ "Y","OOOO","ROOO","OOOOOOOOOOO","YOOO"},
+                new String[]{"Y", "OOOO", "ROOO", "OOOOOOOOOOO", "YOOO"},
                 myBerlinClock.getBerlinClock("01:01:01")
         );
     }
